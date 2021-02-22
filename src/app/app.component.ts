37325fe363d85e760b26a54de4740bb891acad61
Reuel D'silva <reuelrds@gmail.com>
2021-02-22 01:46:36 -0600

    Compeleted Login Page and SignUp page Markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'freshfood-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
