1959e227604f58945bb9aba8a5a3fb1f4f3bcd19
Reuel D'silva <reuelrds@gmail.com>
2021-02-23 02:13:32 -0600

    Completed Cart Page Design and Added Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfileComponent } from './views/account/profile/profile.component';
+import { CartComponent } from './views/cart/cart.component';
+import { StoreComponent } from './views/store/store.component';
+
+const routes: Routes = [
+  { path: 'store', component: StoreComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
