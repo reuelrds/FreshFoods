bc9860353cae3e31ab188b716578eb92a217722c
Reuel D'silva <reuelrds@gmail.com>
2023-02-22 12:57:37 -0600

    Compeleted Account Details Page

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  state: string;
+  zipcode: number;
+}
